e7e2d6762b135cf0596ece4a5ba59942e26a3e23
mfarzamalam <mfarzamalam@gmail.com>
2021-01-10 17:25:44 +0500

    working with numbers

---
+++ numbers.py
@@ -0,0 +1,17 @@
+first_num = 4**4
+
+print(first_num)
+
+Age = 23
+
+message = "happy " + str(Age) + "rd! Birthday"
+print(message)
+
+print(5+3)
+print(10-2)
+print(2*4)
+print(16/2)
+
+favourite_number = 44
+message = "Fav numer is " + str(favourite_number)
+print(message)
