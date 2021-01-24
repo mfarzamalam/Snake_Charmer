d3dd90fb83b34e906554d3cec74164fbb07c9551
mfarzamalam <mfarzamalam@gmail.com>
2021-01-24 16:09:37 +0500

    working with tuple

---
+++ dimensions.py
@@ -0,0 +1,17 @@
+dimensions = (500,20)
+print(dimensions)
+
+for dimension in dimensions:
+    print(dimension)
+
+restaurant = ('burgur','pizza','roll','paratha','chai')
+
+for item in restaurant:
+    print(item)
+
+
+print("\nUpdated list:")
+restaurant = ['chai','anda','paratha']
+
+for item in restaurant:
+    print(item)
